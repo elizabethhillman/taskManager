c613c9b7a52c89388d71a441ad30441784aca26b
thien tran <thanhthientran1018@gamil.com>
2021-05-17 22:23:42 -0700

    changing the reminder

--- app/__init__.py
+++ app/__init__.py
@@ -38,23 +38,24 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
 
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        backend="redis://localhost:6379/0",
-        broker="redis://localhost:6379/0"
-    )
-    celery.conf.update(app.config)
+# def make_celery(app):
+#     celery = Celery(
+#         app.import_name,
+#         backend="redis://localhost:6379/0",
+#         broker="redis://localhost:6379/0"
+#     )
+#     celery.conf.update(app.config)
 
-    class ContextTask(celery.Task):
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return self.run(*args, **kwargs)
+#     class ContextTask(celery.Task):
+#         def __call__(self, *args, **kwargs):
+#             with app.app_context():
+#                 return self.run(*args, **kwargs)
 
-    celery.Task = ContextTask
-    return celery
+#     celery.Task = ContextTask
+#     return celery
 
 
-celery = make_celery(app)
+celery = Celery(app.name,broker= 'redis://127.0.0.1:6379/0',)
+
 
 from app import routes, models
